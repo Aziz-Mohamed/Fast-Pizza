5fcc1b540a46b3061a15573c4c66015163577978
abdilaziz Mohamed <abdilaziz.m.elsayed@gmail.com>
2024-08-18 07:42:31 +0300

    feat (cart) : make priority order even after ordering

--- src/features/order/Order.jsx
+++ src/features/order/Order.jsx
@@ -9,6 +9,7 @@ import {
 import { useFetcher, useLoaderData } from "react-router-dom";
 import OrderItem from "../order/OrderItem";
 import { useEffect } from "react";
+import UpdateOrder from "./UpdateOrder";
 
 const order = {
   id: "ABCDEF",
@@ -123,6 +124,7 @@ function Order() {
           To pay on delivery: {formatCurrency(orderPrice + priorityPrice)}
         </p>
       </div>
+      {!priority && <UpdateOrder order={order} />}
     </div>
   );
 }
